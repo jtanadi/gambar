43e88d5c85b9a3c4286a4fa25bc0761b7a6fd4de
Jesen Tanadi <mail@jesentanadi.com>
2019-07-29 09:04:04 -0400

    complete implementation

--- src/undoStack.js
+++ src/undoStack.js
@@ -1,4 +1,4 @@
-class UndoStack {
+module.exports = class UndoStack {
   // Simple implementation of a stack with a limit. Once the limit
   // has been reached we go to the beginning of the stack
 
@@ -14,49 +14,40 @@ class UndoStack {
   }
 
   clear() {
-    // Clear up to this.index only, in case index < length
-    for (let i = 0; i < this.index; i++) {
+    // Iterate over the smaller amount
+    const end = (this.index < this.limit)
+      ? this.index
+      : this.limit
+
+    for (let i = 0; i < end; i++) {
       this.stack[i] = null;
     }
+    this.start = 0;
     this.index = 0;
   }
 
   push(item) {
     if (this.index >= this.limit) {
-      this.start++;
-      this.start = this.start % this.limit;
+      this.start = (this.start + 1) % this.limit;
     }
     this.stack[this.index % this.limit] = item;
     this.index++;
   }
 
   pop() {
-    if (this.index === 0 && this.start === 0) {
-      return null
-    } else if (this.index === 0) {
-      this.index = this.stack.length;
-    }
+    // Don't decrement when index is 0
+    if (!this.index) return null;
 
-    const item = this.stack[--this.index];
-    this.stack[this.index] = null;
+    this.index--;
+    const item = this.stack[this.index % this.limit];
+    this.stack[this.index % this.limit] = null;
 
-    if (this.index === this.start) {
-      this.index = 0;
+    // Reset count
+    if (!this.stack[this.start] && !this.stack[this.index]) {
       this.start = 0;
+      this.index = 0;
     }
 
     return item;
-  }
-
-  shift() {
-    // Shift second half of stack into first half
-    let j = this.limit / 2;
-    for (let i = 0; i < this.limit; i++) {
-      this.stack[i] = (i >= this.limit / 2)
-        ? null
-        : this.stack[j++];
-    }
-    this.start = 0;
-    this.index = 0;
   }
 }
