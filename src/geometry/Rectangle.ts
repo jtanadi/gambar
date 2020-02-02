67b6cb1ae29a299057f3ee16373aaeacb6636e5f
Jesen Tanadi <mail@jesentanadi.com>
2020-02-02 10:52:35 -0500

    clean up

--- src/geometry/Rectangle.ts
+++ src/geometry/Rectangle.ts
@@ -2,10 +2,6 @@ import Point from "./Point"
 import Shape, { PossibleShapes, StyleProps } from "./Shape"
 
 export default class Rectangle extends Shape {
-  start: Point
-  width: number
-  height: number
-
   constructor(pt0: Point, pt1: Point, style: StyleProps) {
     super(pt0, pt1, PossibleShapes.RECTANGLE, style)
 
