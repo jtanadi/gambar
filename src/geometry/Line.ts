edc5c6734f80c24376b01d31ef0e32fceae6bb89
Jesen Tanadi <mail@jesentanadi.com>
2020-02-03 08:08:18 -0500

    implement move method

--- src/geometry/Line.ts
+++ src/geometry/Line.ts
@@ -9,7 +9,16 @@ export default class Line extends Shape {
     super(pt0, pt1, PossibleShapes.LINE, style)
     this.start = pt0
     this.end = pt1
+    this.createShape()
+  }
 
+  move(delta: Point): void {
+    this.start = new Point(this.start.x + delta.x, this.start.y + delta.y)
+    this.end = new Point(this.end.x + delta.x, this.end.y + delta.y)
+    this.createShape()
+  }
+
+  protected createShape(): void {
     this.path = new Path2D()
     this.path.moveTo(this.start.x, this.start.y)
     this.path.lineTo(this.end.x, this.end.y)
